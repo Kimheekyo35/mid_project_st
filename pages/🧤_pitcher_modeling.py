0928a892b3dbe96a55e547a0e996052d83927b67
unknown <khkyo010305D@gmail.com>
2024-09-04 18:58:07 +0900

    first commit

--- pages/🧤_pitcher_modeling.py
+++ pages/🧤_pitcher_modeling.py
@@ -69,7 +69,7 @@ def run_ml_app():
         st.write(f"SO_G_value: {SO_G_value}")
         st.write(f"QS_G_value: {QS_G_value}")
         st.write(f"exp_QS_value: {exp_QS_value}")
-        st.write(f"K_BB_value: {K_BB_value}")
+        st.write(f"K_BB_value: {round(K_BB_value,2)}")
         st.write(f"NP_IP_value:{round(NP_IP_value,2)}")
         st.write(f"K-BB_value: {round(KBB_value,2)}")
         
